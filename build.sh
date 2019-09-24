93d52d384b144830aa6d04f35faa1c131c6bb498
young na <ygna91@gmail.com>
2019-09-24 09:03:50 +0900

    Refactor : refactor build.sh to copy only necessary files to docs directory

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 gitbook build
-sudo cp -R _book/* docs/
+sudo cp -R _book/Contents docs/
+sudo cp -R _book/gitbook docs/
+sudo cp -R _book/images docs/
+sudo cp -R _book/index.html docs/
+sudo cp -R _book/search_index.json docs/
